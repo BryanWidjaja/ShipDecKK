a30402532be393fbb61b92afff49b03e4302b14c
Bryan Widjaja <crz.github@gmail.com>
2025-05-12 13:05:30 +0700

    completed form validations

--- components/subscription_form.js
+++ components/subscription_form.js
@@ -1,14 +1,18 @@
 const subscriptionForm = document.querySelector(".subscription-form");
 
-const nameInput = document.getElementById("name");
-const emailInput = document.getElementById("email");
-const passwordInput = document.getElementById("password");
-const termsInput = document.getElementById("terms");
-const privacyInput = document.getElementById("privacy");
+const nameInput = document.getElementById("name-input");
+const emailInput = document.getElementById("email-input");
+const passwordInput = document.getElementById("password-input");
+const ageInput = document.getElementById("age-input");
+const interestsInput = document.querySelectorAll("#interests-input");
+const termsInput = document.getElementById("terms-input");
+const privacyInput = document.getElementById("privacy-input");
 
 const nameError = document.getElementById("name-error");
 const emailError = document.getElementById("email-error");
 const passwordError = document.getElementById("password-error");
+const ageError = document.getElementById("age-error");
+const interestsError = document.getElementById("interests-error");
 const termsError = document.getElementById("terms-error");
 const privacyError = document.getElementById("privacy-error");
 
@@ -27,10 +31,34 @@ subscriptionForm.addEventListener("submit", function (e) {
   if (!emailInput.value.trim()) {
     displayErrorMsg(emailError, "Please enter your email!");
     valid = false;
-  } else valid = valid && validEmail(emailInput.value.trim());
+  } else valid = validEmail(emailInput.value.trim());
 
   if (!passwordInput.value.trim()) {
     displayErrorMsg(passwordError, "Please enter your password!");
+    valid = false;
+  } else valid = validPassword(passwordInput.value.trim());
+
+  const age = Number(ageInput.value.trim());
+
+  if (isNaN(age)) {
+    displayErrorMsg(ageError, "Please enter a number!");
+    valid = false;
+  } else if (age < 18) {
+    displayErrorMsg(ageError, "Age must be above 18!");
+    valid = false;
+  } else if (age > 99) {
+    displayErrorMsg(ageError, "Age must be below 99!");
+    valid = false;
+  }
+
+  let interestChecked = false;
+
+  interestsInput.forEach((checkbox) => {
+    if (checkbox.checked) interestChecked = true;
+  });
+
+  if (!interestChecked) {
+    displayErrorMsg(interestsError, "Please select at least one interest");
     valid = false;
   }
 
@@ -59,6 +87,8 @@ const resetAllErrorMsg = () => {
   nameError.textContent = "";
   emailError.textContent = "";
   passwordError.textContent = "";
+  ageError.textContent = "";
+  interestsError.textContent = "";
   termsError.textContent = "";
   privacyError.textContent = "";
 };
@@ -89,3 +119,67 @@ showPassBtn.addEventListener("click", function () {
     showPassBtn.src = "../assets/icons/show.png";
   }
 });
+
+const validPassword = (passwordInput) => {
+  console.log("Validating password:", passwordInput);
+  if (passwordInput.length < 8) {
+    displayErrorMsg(
+      passwordError,
+      "Password must be longer than 8 characters!"
+    );
+    return false;
+  }
+
+  if (!hasUppercase(passwordInput)) {
+    displayErrorMsg(
+      passwordError,
+      "Password must contain an uppercase letter!"
+    );
+    return false;
+  }
+
+  if (!hasNumber(passwordInput)) {
+    displayErrorMsg(passwordError, "Password must contain a number!");
+    return false;
+  }
+
+  if (hasSpecialChars(passwordInput)) {
+    displayErrorMsg(
+      passwordError,
+      "Password musn't contain special characters!"
+    );
+    return false;
+  }
+
+  return true;
+};
+
+const hasSpecialChars = (passwordInput) => {
+  const specialChars = [
+    '"',
+    "'",
+    "\\",
+    "/",
+    ";",
+    ":",
+    "&",
+    "<",
+    ">",
+    "`",
+    "%",
+    "*",
+    "?",
+    "^",
+    "~",
+  ];
+
+  return specialChars.some((char) => passwordInput.includes(char));
+};
+
+const hasUppercase = (passwordInput) => {
+  return [...passwordInput].some((char) => char >= "A" && char <= "Z");
+};
+
+const hasNumber = (passwordInput) => {
+  return [...passwordInput].some((char) => char >= "0" && char <= "9");
+};
