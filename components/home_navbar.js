b4aa7613c91e757963b935a9fcf8891d885cb2b7
Bryan Widjaja <crz.github@gmail.com>
2025-05-16 11:40:52 +0700

    fix hamburger

--- components/home_navbar.js
+++ components/home_navbar.js
@@ -19,3 +19,16 @@ window.addEventListener("scroll", function () {
   }
   lastScrollY = currentScrollY;
 });
+
+const hamburgerBtn = document.getElementById("hamburger-btn");
+const linksContainer = document.querySelector(".links");
+
+const hamburgerIcon = "./assets/icons/hamburger.svg";
+const closeIcon = "./assets/icons/white_close.svg";
+
+hamburgerBtn.addEventListener("click", () => {
+  const isMenuVisible = linksContainer.classList.toggle("visible");
+  navBar.classList.toggle("visible");
+
+  hamburgerBtn.src = isMenuVisible ? closeIcon : hamburgerIcon;
+});
