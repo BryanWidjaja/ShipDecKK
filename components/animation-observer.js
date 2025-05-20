57832f7567a74168dd7b915ea8f8f9f5eee607e4
Bryan Widjaja <crz.github@gmail.com>
2025-05-20 14:20:32 +0700

    finishing landing

--- components/animation-observer.js
+++ components/animation-observer.js
@@ -2,11 +2,12 @@ const observer = new IntersectionObserver((entries) => {
   entries.forEach((entry) => {
     if (entry.isIntersecting) {
       entry.target.classList.add("show");
-    } else {
-      entry.target.classList.remove("show");
+      observer.unobserve(entry.target);
     }
   });
 });
 
-const hiddenElements = document.querySelectorAll(".hidden-down, .hidden-left");
+const hiddenElements = document.querySelectorAll(
+  ".hidden-down, .hidden-left, .hidden-right"
+);
 hiddenElements.forEach((element) => observer.observe(element));
---
+++ components/faq.js
@@ -0,0 +1,14 @@
+const questionContainers = document.querySelectorAll(".faq-question-container");
+
+const plusIcon = "../assets/icons/plus.svg";
+const minusIcon = "../assets/icons/minus.svg";
+
+questionContainers.forEach((question) => {
+  question.addEventListener("click", () => {
+    const answer = question.nextElementSibling;
+    const faqBtn = question.querySelector("img");
+
+    const isAnswerVisible = answer.classList.toggle("visible");
+    faqBtn.src = isAnswerVisible ? minusIcon : plusIcon;
+  });
+});
